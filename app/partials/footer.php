260a9e6a05c993d102454582a582994c3b25c9ca
Raymond Luna <lunaraymondalvar@gmail.com>
2019-01-10 16:40:42 +0800

    ecommerce activity

---
+++ app/partials/footer.php
@@ -0,0 +1,8 @@
+
+<footer class="row bg-success fixed-bottom px-4 justify-content-center">
+	<small class="text-center">
+		mystore | Raymond Luna | &copy;2019
+	</small>
+</footer>
+
+<script type="../assets/js/script.js"></script>
---
+++ app/views/home.php
@@ -0,0 +1,13 @@
+
+<?php require_once '../partials/template.php'; ?>
+
+<?php function get_page_content() { ?>
+
+	<div class="container-fluid">
+		<div class="jumbotron bg-primary text-center text-white mt-3">
+			<h1>Mystore</h1>
+			<p>Your favorite store</p>
+		</div>	<!-- end of jumbotron -->	
+	</div> <!-- end of container -->
+
+<?php } ?>
